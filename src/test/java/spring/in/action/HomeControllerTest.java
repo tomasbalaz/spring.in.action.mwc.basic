d3e5c9206ca3a805163aab73ed530011002591e2
tomas.balaz <tomas.balaz@asseco-ce.com>
2018-10-09 14:17:44 +0200

    Update Junit class with shouldShowRecentSpittles test

--- src/test/java/spring/in/action/HomeControllerTest.java
+++ src/test/java/spring/in/action/HomeControllerTest.java
@@ -4,10 +4,22 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.view.InternalResourceView;
+
 import spring.in.action.mwc.basic.controller.HomeController;
+import spring.in.action.mwc.basic.controller.SpittleController;
+import spring.in.action.mwc.basic.model.Spittle;
+import spring.in.action.mwc.basic.service.SpittleRepository;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class HomeControllerTest {
 
@@ -25,4 +37,27 @@ public class HomeControllerTest {
 		mockMvc.perform(get("/")).andExpect(view().name("home"));
 	}
 
+	@Test
+	public void shouldShowRecentSpittles() throws Exception {
+		List<Spittle> expectedSpittles = createSpittleList(20);
+		SpittleRepository mockRepository = mock(SpittleRepository.class);
+		when(mockRepository.findSpittles(Long.MAX_VALUE, 20)).thenReturn(expectedSpittles);
+
+		SpittleController controller = new SpittleController(mockRepository);
+
+		MockMvc mockMvc = standaloneSetup(controller)
+				.setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp")).build();
+
+		mockMvc.perform(get("/spittles")).andExpect(view().name("spittles"))
+				.andExpect(model().attributeExists("spittleList"))
+				.andExpect(model().attribute("spittleList", hasItems(expectedSpittles.toArray())));
+	}
+
+	private List<Spittle> createSpittleList(int count) {
+		List<Spittle> spittles = new ArrayList<Spittle>();
+		for (int i = 0; i < count; i++) {
+			spittles.add(new Spittle("Spittle " + i, new Date()));
+		}
+		return spittles;
+	}
 }
