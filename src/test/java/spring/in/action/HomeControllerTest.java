d004da06c0f2b059da4732cb71a34ddbc5b1e2cf
tomas.balaz <tomas.balaz@asseco-ce.com>
2018-10-09 08:10:42 +0200

    Create HomeControllerTest Junit test

---
+++ src/test/java/spring/in/action/HomeControllerTest.java
@@ -0,0 +1,28 @@
+package spring.in.action;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import spring.in.action.mwc.basic.controller.HomeController;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+public class HomeControllerTest {
+
+	@Test
+	public void testHomePage() throws Exception {
+		HomeController controller = new HomeController();
+		assertEquals("home", controller.home());
+	}
+
+	@Test
+	public void testHomePageAsMock() throws Exception {
+		HomeController controller = new HomeController();
+		MockMvc mockMvc = standaloneSetup(controller).build();
+
+		mockMvc.perform(get("/")).andExpect(view().name("home"));
+	}
+
+}
